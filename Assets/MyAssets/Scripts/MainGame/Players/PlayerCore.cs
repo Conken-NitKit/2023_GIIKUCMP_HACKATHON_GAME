1244979bb7ef090e6e459a8948426e83c96cda24
woods-one <0820ricer@gmail.com>
2023-06-16 15:58:12 +0900

    追加 : GameStateの追加

--- Assets/MyAssets/Scripts/MainGame/Players/PlayerCore.cs
+++ Assets/MyAssets/Scripts/MainGame/Players/PlayerCore.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCore : MonoBehaviour
+namespace Assets.MyAssets.Scripts.MainGame.Players
 {
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerCore : MonoBehaviour
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private PlayerParameter _parameter;
     }
 }
