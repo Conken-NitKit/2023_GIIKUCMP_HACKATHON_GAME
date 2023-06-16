6da78b6ea90c3a53114205ad30e331431dadefdc
woods-one <0820ricer@gmail.com>
2023-06-16 15:26:26 +0900

    追加 : カードの引きなおしの機能実装

--- Assets/MyAssets/Scripts/MainGame/Cards/CardDisPlay.cs
+++ Assets/MyAssets/Scripts/MainGame/Cards/CardDisPlay.cs
@@ -8,7 +8,10 @@ namespace Assets.MyAssets.Scripts.MainGame.Cards
     public class CardDisPlay : MonoBehaviour
     {
         private const int DEFULT_CARD_NUM = 10;
-        private TextMeshPro[] _cardTMPs = new TextMeshPro[DEFULT_CARD_NUM];
+        private ReactiveCollection<string> _cardTexts = new ReactiveCollection<string>();
+        private TextMeshPro[] _cards = new TextMeshPro[DEFULT_CARD_NUM];
+        
+        private Vector2[] _cardPositions = new Vector2[10];
         
         [SerializeField]
         private CardDB _cardDb;
@@ -16,14 +19,37 @@ namespace Assets.MyAssets.Scripts.MainGame.Cards
         private GameObject _cardObject;
 
         public void DrawFirst()
-        {
-            var _drawCards = _cardDb.DrawCards(DEFULT_CARD_NUM);
+        {            
+            _cardTexts.ObserveReplace().Subscribe(replaceEvent =>
+             {
+                 _cards[replaceEvent.Index].text = _cardTexts[replaceEvent.Index];
+             });
+            
+            var drawCards = _cardDb.DrawCards(DEFULT_CARD_NUM);
             for (int i = 0; i < DEFULT_CARD_NUM; i++)
             {
-                _cardTMPs[i] = Instantiate(_cardObject, new Vector2(-6.75f + i * 1.5f,-4f), Quaternion.identity)
-                    .GetComponentInChildren<TextMeshPro>();
-                _cardTMPs[i].text = _drawCards[i];
+                _cardPositions[i] = new Vector2(-6.75f + i * 1.5f, -4f);
+                var cards = Instantiate(_cardObject, _cardPositions[i], Quaternion.identity);
+                _cards[i] = cards.GetComponentInChildren<TextMeshPro>();
+                cards.GetComponent<CardInfo>().CardNum = i;
+                _cardTexts.Add(_cards[i].text);
+                _cardTexts[i] = drawCards[i];
             }
+        }
+        
+        public void ReDrawAllCard()
+        {
+            var drawCards = _cardDb.DrawCards(DEFULT_CARD_NUM);
+            for (int i = 0; i < DEFULT_CARD_NUM; i++)
+            {
+                _cardTexts[i] = drawCards[i];
+            }
+        }
+
+        public void ReDrawCard(int cardIndex)
+        {
+            var drawCards = _cardDb.DrawCards(1);
+            _cardTexts[cardIndex] = drawCards[0];
         }
     }
 }
--- Assets/MyAssets/Scripts/MainGame/Cards/CardInfo.cs
+++ Assets/MyAssets/Scripts/MainGame/Cards/CardInfo.cs
@@ -8,11 +8,14 @@ namespace Assets.MyAssets.Scripts.MainGame.Cards
         [SerializeField]
         private TextMeshPro _cardText;
 
+        public int CardNum;
+
         public string CardText
         {
             //ここで今カードが推せるか助詞のカードが推せるかの判定をする
             get
             {
+                _cardText.text = "";
                 return _cardText.text;
             }
         }
